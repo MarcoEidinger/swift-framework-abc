cb9bfede314c6c54ad79e1dc06ec4749f8c7d391
Marco Eidinger <marco.eidinger@sap.com>
2020-05-01 11:59:16 -0700

    add compiled version for 1.0

---
+++ CompiledVersions/1.0/Products/Debug-iphonesimulator/SwiftFrameworkAbc.framework/Headers/SwiftFrameworkAbc.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFrameworkAbc.h
+//  SwiftFrameworkAbc
+//
+//  Created by Eidinger, Marco on 5/1/20.
+//  Copyright © 2020 Eidinger, Marco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFrameworkAbc.
+FOUNDATION_EXPORT double SwiftFrameworkAbcVersionNumber;
+
+//! Project version string for SwiftFrameworkAbc.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkAbcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFrameworkAbc/PublicHeader.h>
+
+
